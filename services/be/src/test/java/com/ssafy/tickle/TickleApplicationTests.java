678f0badd051a0698dbcdf6f7b7575a67a72636d
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-04-20 11:39:40 +0900

    [BE] 148 chore: 스프링 부트 프로젝트 초기 설정

---
+++ services/be/src/test/java/com/ssafy/tickle/TickleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tickle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
