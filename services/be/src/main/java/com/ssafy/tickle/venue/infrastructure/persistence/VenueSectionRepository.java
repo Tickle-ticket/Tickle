02a003a9531d6df423a51043724f565a064a0a72
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-04-20 12:38:33 +0900

    [BE] 148 feat: 공연장 관련 repository 구현

---
+++ services/be/src/main/java/com/ssafy/tickle/venue/infrastructure/persistence/VenueSectionRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.tickle.venue.infrastructure.persistence;
+
+import com.ssafy.tickle.venue.domain.VenueSection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 공연장 구역 엔티티를 조회하고 저장하는 JPA 리포지토리입니다.
+ */
+public interface VenueSectionRepository extends JpaRepository<VenueSection, Long> {
+}
