4ecc31a09bcefe241de807a71cbeba25d4226cb3
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-04-21 10:14:07 +0900

    [BE] 93 feat: 공연 검색 API 및 DTO 추가

--- services/be/src/main/java/com/ssafy/tickle/event/presentation/EventApiDoc.java
+++ services/be/src/main/java/com/ssafy/tickle/event/presentation/EventApiDoc.java
@@ -2,6 +2,7 @@ package com.ssafy.tickle.event.presentation;
 
 import com.ssafy.tickle.common.response.BaseResponse;
 import com.ssafy.tickle.event.presentation.dto.EventDetailResponse;
+import com.ssafy.tickle.event.presentation.dto.EventListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -13,6 +14,30 @@ import org.springframework.http.ResponseEntity;
  */
 @Tag(name = "Event", description = "공연 이벤트 조회 API")
 public interface EventApiDoc {
+
+    /**
+     * 이벤트 목록 검색 API 문서 정의입니다.
+     *
+     * @param keyword 제목 검색어
+     * @param categoryId 카테고리 식별자
+     * @param page 페이지 번호
+     * @param size 페이지 크기
+     * @return 이벤트 목록 응답
+     */
+    @Operation(
+            summary = "이벤트 목록 조회",
+            description = "전체 목록, 제목 검색, 카테고리 검색 조건으로 이벤트 목록을 조회합니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "이벤트 목록 조회 성공"
+    )
+    ResponseEntity<BaseResponse<EventListResponse>> getEvents(
+            @Parameter(description = "이벤트 제목 검색어") String keyword,
+            @Parameter(description = "카테고리 식별자") Long categoryId,
+            @Parameter(description = "페이지 번호", example = "0") int page,
+            @Parameter(description = "페이지 크기", example = "20") int size
+    );
 
     /**
      * 이벤트 상세 조회 API 문서 정의입니다.
