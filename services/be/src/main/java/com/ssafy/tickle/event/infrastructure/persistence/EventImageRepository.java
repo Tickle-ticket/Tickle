ce8dd04f3c860f95ce037cf62538e203fc46eaa0
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-04-21 10:15:48 +0900

    [BE] 93 feat: 공연 이미지 검색 조회 메서드 추가

--- services/be/src/main/java/com/ssafy/tickle/event/infrastructure/persistence/EventImageRepository.java
+++ services/be/src/main/java/com/ssafy/tickle/event/infrastructure/persistence/EventImageRepository.java
@@ -17,4 +17,16 @@ public interface EventImageRepository extends JpaRepository<EventImage, Long> {
      * @return 이미지 목록
      */
     List<EventImage> findByEvent_IdOrderByDisplayOrderAsc(Long eventId);
+
+    /**
+     * 여러 이벤트의 특정 타입 이미지를 이벤트별, 노출 순서별로 조회합니다.
+     *
+     * @param eventIds 이벤트 식별자 목록
+     * @param imageType 이미지 타입
+     * @return 이미지 목록
+     */
+    List<EventImage> findByEvent_IdInAndImageTypeOrderByEvent_IdAscDisplayOrderAsc(
+            List<Long> eventIds,
+            EventImage.ImageType imageType
+    );
 }
