6050bfb3db71aadee0282a378b76f67f3dfa83da
양희령 <gaau511@gmail.com>
2026-04-24 13:19:34 +0900

    [BE] 151 feat: 카테고리 별 공연 랭킹 조회 API 구현

--- services/be/src/main/java/com/ssafy/tickle/event/presentation/EventController.java
+++ services/be/src/main/java/com/ssafy/tickle/event/presentation/EventController.java
@@ -1,8 +1,8 @@
 package com.ssafy.tickle.event.presentation;
 
-import com.ssafy.tickle.common.exception.code.SuccessCode;
 import com.ssafy.tickle.common.response.BaseResponse;
 import com.ssafy.tickle.event.application.EventService;
+import com.ssafy.tickle.event.presentation.dto.CategoryRankingResponse;
 import com.ssafy.tickle.event.presentation.dto.EventDetailResponse;
 import com.ssafy.tickle.event.presentation.dto.EventListResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 이벤트 상세 조회 API를 제공하는 컨트롤러입니다.
@@ -43,6 +45,22 @@ public class EventController implements EventApiDoc {
         return ResponseEntity
                 .ok()
                 .body(BaseResponse.success(eventService.getEvents(keyword, categoryId, page, size)));
+    }
+
+    /**
+     * 랭킹 TOP5를 조회합니다.
+     *
+     * @param categoryId 카테고리 식별자(없으면 전체)
+     * @return 랭킹 응답
+     */
+    @Override
+    @GetMapping("/ranking")
+    public ResponseEntity<BaseResponse<CategoryRankingResponse>> getRanking(
+            @RequestParam(required = false) Long categoryId
+    ) {
+        return ResponseEntity
+                .ok()
+                .body(BaseResponse.success(eventService.getRanking(categoryId)));
     }
 
     /**
