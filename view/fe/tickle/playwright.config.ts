91ed662a6a7e8cb0aa1e2cc8fa4c87a7c2b533f3
gyqls080813 <124768918+gyqls080813@users.noreply.github.com>
2026-04-28 17:50:39 +0900

    [FE] feat: build 실패 수정

--- view/fe/tickle/playwright.config.ts
+++ view/fe/tickle/playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm start',
-    url: 'http://localhost:3000',
+    url: 'https://k14a203.p.ssafy.io/',
     reuseExistingServer: !process.env.CI,
   },
 });
