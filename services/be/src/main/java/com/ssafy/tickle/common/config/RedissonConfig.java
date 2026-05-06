cd6b8fa75fc20d77cb072575e2c8695ec33b4d48
Willy <willy_1020@naver.com>
2026-05-06 17:45:22 +0900

    [BE] fix: Redis 연결 방식을 Sentinel에서 Single Server로 변경 및 Docker 환경 변수 추가

--- services/be/src/main/java/com/ssafy/tickle/common/config/RedissonConfig.java
+++ services/be/src/main/java/com/ssafy/tickle/common/config/RedissonConfig.java
@@ -3,34 +3,33 @@ package com.ssafy.tickle.common.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.SentinelServersConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
     @Bean(destroyMethod = "shutdown")
-    public RedissonClient redissonClient(
-            @Value("${spring.data.redis.sentinel.master}") String masterName,
-            @Value("${spring.data.redis.sentinel.nodes}") String sentinelNodes,
-            @Value("${spring.data.redis.password:}") String password
-    ) {
+    public RedissonClient redissonClient() {
         Config config = new Config();
-
-        String[] addresses = Arrays.stream(sentinelNodes.split(","))
-                .map(node -> "redis://" + node.trim())
-                .toArray(String[]::new);
-
-        SentinelServersConfig sentinelConfig = config.useSentinelServers()
-                .setMasterName(masterName)
-                .addSentinelAddress(addresses);
+        
+        // 단일 서버 모드로 설정
+        String address = "redis://" + redisHost + ":" + redisPort;
+        config.useSingleServer()
+                .setAddress(address);
 
         if (password != null && !password.isBlank()) {
-            sentinelConfig.setPassword(password);
+            config.useSingleServer().setPassword(password);
         }
 
         return Redisson.create(config);
