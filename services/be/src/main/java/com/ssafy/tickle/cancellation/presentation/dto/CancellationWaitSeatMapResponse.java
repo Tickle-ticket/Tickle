888df92371bb150f142696b7b43a981f28717de5
양희령 <gaau511@gmail.com>
2026-05-09 13:26:23 +0900

    [BE] 350 feat: 예매 대기 좌석맵 응답에 venueId 추가

--- services/be/src/main/java/com/ssafy/tickle/cancellation/presentation/dto/CancellationWaitSeatMapResponse.java
+++ services/be/src/main/java/com/ssafy/tickle/cancellation/presentation/dto/CancellationWaitSeatMapResponse.java
@@ -5,9 +5,11 @@ import java.util.List;
 /**
  * 예매 대기 페이지용 회차 좌석 응답 DTO입니다.
  *
+ * @param venueId 공연장 식별자
  * @param sections 구역별 좌석 목록
  */
 public record CancellationWaitSeatMapResponse(
+        Long venueId,
         List<CancellationWaitSeatSectionResponse> sections
 ) {
 }
