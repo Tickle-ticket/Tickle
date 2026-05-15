193aa26d282c67e64ce3919f9136122de29b73d5
양희령 <gaau511@gmail.com>
2026-05-15 11:34:33 +0900

    [BE] 449 test: 테스트 데이터 추가

--- services/be/src/main/java/com/ssafy/tickle/reservation/application/BookingPreorderService.java
+++ services/be/src/main/java/com/ssafy/tickle/reservation/application/BookingPreorderService.java
@@ -45,7 +45,7 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class BookingPreorderService {
 
-    private static final Long start = 164381L;
+    private static final Long start = 20741L;
     private static final List<Long> MOCK_WIN_SESSION_SEAT_IDS = List.of(
             start,
             start+1,
