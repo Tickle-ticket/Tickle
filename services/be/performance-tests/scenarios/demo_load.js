02bab578bab2bb8c6f6667d1f795834e106bf467
Willy <willy_1020@naver.com>
2026-05-21 01:03:59 +0900

    [BE] chore: demo_load.js 30초 100명 / 1분 500명으로 원복

--- services/be/performance-tests/scenarios/demo_load.js
+++ services/be/performance-tests/scenarios/demo_load.js
@@ -16,8 +16,8 @@ export const options = {
       executor: "ramping-vus",
       startVUs: 1,
       stages: [
-        { duration: "30s", target: 200  },
-        { duration: "1m",  target: 1000 },
+        { duration: "30s", target: 100 },
+        { duration: "1m",  target: 500 },
         { duration: "1m",  target: 1   },
       ],
     },
